60355bf77ac7c98ce3ecc3f4ced7a1e2ed2e116e
liaodengbin <2122492160@qq.com>
2023-04-09 00:02:44 +0800

    登陆注册界面

--- app/src/main/java/com/example/funproject/MainActivity.java
+++ app/src/main/java/com/example/funproject/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.funproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.funproject.activity.LoginActivity;
+import com.example.funproject.activity.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button loginButton ;
+    private Button registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.btn_login);
+        registerButton = findViewById(R.id.btn_register);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inToLogin  = new Intent(MainActivity.this, LoginActivity.class);
+
+                startActivity(inToLogin);
+            }
+        });
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intoRegister = new Intent(MainActivity.this, RegisterActivity.class);
+
+                startActivity(intoRegister);
+            }
+        });
     }
 }
